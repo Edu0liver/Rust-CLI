9f59da10acbc334ebc9da329d869b61e21f2758d
Eduardo Oliveira Alves <eduardo.alves@icc.org.br>
2022-12-05 11:35:31 -0300

    indentação

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+#![allow(dead_code)]
+
 use std::env;
 use std::io::{self, Write};
 use std::net::{IpAddr, TcpStream};
@@ -7,7 +9,6 @@ use std::sync::mpsc::{Sender, channel};
 use std::thread;
 
 const MAX: u16 = 65535;
-
 struct Arguments {
     flag: String, 
     ipaddr: IpAddr, 
@@ -25,22 +26,29 @@ impl Arguments {
         let f = args[1].clone();
 
         if let Ok(ipaddr) = IpAddr::from_str(&f) {
+
             return Ok(Arguments {
                 flag: String::from(""),
                 ipaddr,
                 threads: 4
             });
+
         } else {
+
             let flag = args[1].clone();
             
             if flag.contains("-h") || flag.contains("-help") && args.len() == 2 {
+
                 println!("Usage: -j to select how many threads you want\n\r-h or -help to show this help message");
+
                 return Err("help");
 
             } else if flag.contains("-h") || flag.contains("-help") {
+
                 return Err("Too many arguments");
 
             } else if flag.contains("-j") {
+
                 let ipaddr = match IpAddr::from_str(&args[3]) {
                     Ok(s) => s,
                     Err(_) => return Err("Not a valid IPADDR; must be IPv4 or IPv6")
@@ -50,6 +58,7 @@ impl Arguments {
                     Ok(s) => s,
                     Err(_) => return Err("Failed to parse thread number")
                 };
+
                 return Ok(Arguments {
                     threads,
                     flag,
@@ -65,6 +74,7 @@ impl Arguments {
 }
 
 fn scan(tx: Sender<u16>, start_port: u16, addr: IpAddr, num_threads: u16) {
+
     let mut port: u16 = start_port + 1;
 
     loop {
@@ -75,19 +85,21 @@ fn scan(tx: Sender<u16>, start_port: u16, addr: IpAddr, num_threads: u16) {
                 tx.send(port).unwrap();
             }
             Err(_) => {}    
-            
         }
 
         if (MAX - port) <= num_threads {
             break;
         }
+
         port += num_threads;
+
     }
 }
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     let program = args[0].clone();
+
     let arguments = Arguments::new(&args).unwrap_or_else(
         |err| {
             if err.contains("help") {
@@ -114,6 +126,7 @@ fn main() {
     let mut out = vec![];
 
     drop(tx);
+    
     for p in rx {
         out.push(p);
     }    
